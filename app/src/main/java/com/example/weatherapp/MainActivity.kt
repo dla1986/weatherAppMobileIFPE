fd7e26137e7d3734c2bc488cd4ace2414ef4384c
dlassuncao <dla1986@hotmail.com>
2026-05-15 16:55:00 -0300

    Pratica 2 parte 1 até o passo 5.

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,20 +1,9 @@
 package com.example.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
 
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-
-import androidx.compose.runtime.Composable
-
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 import com.example.weatherapp.ui.theme.WeatherAPPTheme
 
@@ -36,38 +25,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-
-    val activity = LocalContext.current as Activity
-
-    Column(
-        modifier = modifier
-            .padding(24.dp)
-            .fillMaxSize()
-    ) {
-
-        Text(
-            text = "Bem-vindo ao sistema!",
-            fontSize = 24.sp
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-
-        // Botão sair
-
-
-        Button(
-            onClick = {
-
-                activity.finish()
-
-            }
-        ) {
-
-            Text("Sair")
-
-        }
-    }
-}
