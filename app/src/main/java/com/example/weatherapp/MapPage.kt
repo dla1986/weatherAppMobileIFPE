909532b493e57c9564590eb7d2ea3876a27a7080
dlassuncao <dla1986@hotmail.com>
2026-06-11 19:54:47 -0300

    Atividade 4 Parte 3 passo 1 ao passo 7

--- app/src/main/java/com/example/weatherapp/MapPage.kt
+++ app/src/main/java/com/example/weatherapp/MapPage.kt
@@ -22,35 +22,84 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 //import androidx.compose.material3.Text
+import androidx.compose.runtime.remember
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapPage(modifier: Modifier = Modifier.Companion, viewModel: MainViewModel) {
+    val recife = remember {
+        MarkerState(position = LatLng(-8.05, -34.90))
+    }
+
+    val caruaru = remember {
+        MarkerState(position = LatLng(-8.27, -35.98))
+    }
+
+    val joaopessoa = remember {
+        MarkerState(position = LatLng(-7.12, -34.84))
+    }
+
+    val camPosState = rememberCameraPositionState()
+
+    GoogleMap(
+        modifier = modifier.fillMaxSize(),
+        cameraPositionState = camPosState,
+
+        onMapClick = {
+
+            viewModel.add(
+                "Cidade(${it.latitude}, ${it.longitude})",
+                it
+            )
+        }
 
 
-
-
-    Column(
-        modifier = modifier.fillMaxSize()
-            .background(Color.Gray)
-            .wrapContentSize(Alignment.Center)
     ) {
 
-        Text(
-            text = "Mapa",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+
+        Marker(
+            state = recife,
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_BLUE
+            )
         )
 
-        Spacer(modifier = Modifier.Companion.height(20.dp))
+        Marker(
+            state = caruaru,
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_GREEN
+            )
+        )
 
+        Marker(
+            state = joaopessoa,
+            title = "João Pessoa",
+            snippet = "Marcador em João Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_ORANGE
+            )
+        )
+        viewModel.cities.forEach { city ->
 
+            if (city.location != null) {
 
-
-
-
-
+                Marker(
+                    state = MarkerState(position = city.location),
+                    title = city.name,
+                    snippet = "${city.location}"
+                )
+            }
         }
+    }
+
+
     }
